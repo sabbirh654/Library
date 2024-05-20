3e4427a73c41ce476bd124b882bd10ce87f5985a
Sabbir Hasan <sabbirh1000@gmail.com>
2024-05-20 16:57:28 +0600

    Display book info using card

--- app.js
+++ app.js
@@ -1,74 +1,66 @@
-// const myLibrary = [];
+const myLibrary = [];
 
-// function Book(title, author, pages, readStatus) {
-//     this.title = title;
-//     this.author = author;
-//     this.pages = pages;
-//     this.readStatus = readStatus;
-// }
+function Book(title, author, pages, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
+}
 
-// function addBookToLibrary(book) {
-//     myLibrary.push(book);
-// }
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
 
-// let book1 = new Book('Algorithm', 'Corman', 900, 'not read');
-// let book2 = new Book('AI', 'Sabbir', 600, 'not read');
+let book1 = new Book('Algorithm', 'Corman', 900, 'not read');
+let book2 = new Book('AI', 'Sabbir', 600, 'read');
 
-// addBookToLibrary(book1);
-// addBookToLibrary(book2);
+addBookToLibrary(book1);
+addBookToLibrary(book2);
 
-// function addBookButtonEventHandler() {
-//     const newBook = getFormData();
-//     addBookToLibrary(newBook);
+function populateBooks() {
+    const bookList = document.querySelector('.bookList');
 
-//     showBooks();
-// }
+    for (let book of myLibrary) {
+        const card = createBookCard(book);
+        bookList.append(card);
+    }
+}
 
-// function showBooks() {
+function createBookCard(book) {
+    const card = document.createElement('div');
+    card.className = 'card';
 
-//     const showBookSection = document.querySelector('.show-book-section');
-    
-//     for(let book of myLibrary) {
-//         const card = document.createElement('div');
-//         card.className = 'card';
+    const title = document.createElement('p');
+    title.textContent = `Title : ${book.title}`;
 
-//         const title = document.createElement('p');
-//         title.className = 'bookTitle';
-//         title.textContent = `Title : ${book.title}`;
+    const author = document.createElement('p');
+    author.textContent = `Author : ${book.author}`;
 
-//         const author = document.createElement('p');
-//         author.className = 'bookAuthor';
-//         author.textContent = `Author : ${book.author}`;
+    const pages = document.createElement('p');
+    pages.textContent = `Pages : ${book.pages}`;
 
-//         const pages = document.createElement('p');
-//         pages.className = 'bookPages';
-//         pages.textContent = `Pages : ${book.pages}`;
+    const status = document.createElement('p');
+    status.textContent = `Status : ${book.readStatus}`;
 
-//         const status = document.createElement('p');
-//         status.className = 'bookStatus';
-//         status.textContent = `Status : ${book.readStatus}`;
 
-//         card.appendChild(title);
-//         card.appendChild(author);
-//         card.appendChild(pages);
-//         card.appendChild(status);
+    const bookListButton = document.createElement('div');
+    bookListButton.className = 'bookList-button';
 
-//         showBookSection.appendChild(card);
-//     }
-// }
+    //Change status button
+    const changeButton = document.createElement('button');
+    changeButton.className = 'common-button changeButton';
+    changeButton.textContent = 'Change Status';
 
-// function getFormData() {
-//     const title = document.querySelector('#title').value;
-//     const author = document.querySelector('#author').value;
-//     const pages = document.querySelector('#pages').value;
-//     const status = document.querySelector('#status').value;
+    //Remove Button
+    const removeButton = document.createElement('button');
+    removeButton.className = 'common-button removeButton';
+    removeButton.textContent = 'Remove';
 
-//     const newBook = new Book(title, author, pages, status);
+    bookListButton.append(changeButton, removeButton);
 
-//     return newBook;
-// }
+    card.append(title, author, pages, status, bookListButton);
 
-// const addBookButton = document.querySelector('.addBookButton');
-// addBookButton.addEventListener('click', addBookButtonEventHandler);
+    return card;
+}
 
-// showBooks();
+populateBooks();
